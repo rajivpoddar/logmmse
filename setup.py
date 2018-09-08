29139ec376f88f9d78bf96c0d12d304de14601be
Wilson Ching <wc00160@surrey.ac.uk>
2018-09-08 12:07:53 -0400

    updated long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='logmmse',
-    version='1.1',
+    version='1.2',
     description='A python implementation of the LogMMSE speech enhancement/noise reduction alogrithm',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     url='https://github.com/wilsonchingg/logmmse',
     packages=['logmmse'],
