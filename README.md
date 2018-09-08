2ec7b8c6e5ef13172364ee8fb0b0cc727b882c5a
Wilson Ching <wc00160@surrey.ac.uk>
2018-09-07 22:17:52 -0400

    update docs, add more options

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# Fork information
+The fork improved on the original version to support Python 3, fix a few bugs, and make it importable from other Python scripts
 
-This fork fixed an integer bug on the algorithm, and most importantly, make the file importable
-
-# logmmse
+### logmmse ###
 A python implementation of the LogMMSE speech enhancement/noise reduction alogrithm. This file has been ported from logmmse.m file found at the following link.
+
+### API Reference ###
+
+##### run_logmmse(input_filename, output_filename, initial_noise=6, window_size=0, noise_threshold=0.15, l_trim=False) #####
+
+**input_filename (Mandatory)**
+filename of the wave input
+supported format: 32-bit floating-point, 32-bit PCM, 16-bit PCM, and 8-bit PCM, see https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html     
+
+**output_filename (Mandatory)**
+filename of the wave output
+the format would be the same as input     
+
+**l_trim**
+Whether to remove zeros at the beginning of an array, set this option to true if you encounter the following error:
+```
+sig = sig * hw
+FloatingPointError: invalid value encountered in multiply
+```
+
+### Reference ###
 
 https://raw.githubusercontent.com/braindead/Noise-reduction/master/logmmse.m
--- logmmse.py
+++ logmmse.py
@@ -4,15 +4,18 @@ from __future__ import division
 import numpy as np
 from scipy.io.wavfile import read, write
 from core import logmmse
-from utils import type_conversion
+from utils import to_float, from_float
 
 np.seterr('raise')
 
-
-def run_logmmse(input_filename, output_filename, initial_noise=6, window_size=0, noise_threshold=0.15):
+def run_logmmse(input_filename, output_filename, initial_noise=6, window_size=0, noise_threshold=0.15,
+                l_trim=False):
     fs, input_file = read(input_filename, 'r')
+    # Left trim to prevent an error from the algorithm
+    if l_trim:
+        input_file = np.trim_zeros(input_file, 'f')
     output = np.array([], dtype=input_file.dtype)
-    input_file = type_conversion(input_file)
+    input_file, dtype = to_float(input_file)
     num_frames = len(input_file)
 
     chunk_size = int(np.floor(60*fs))
@@ -23,7 +26,7 @@ def run_logmmse(input_filename, output_filename, initial_noise=6, window_size=0,
         signal = input_file[frames_read:frames_read + frames]
         frames_read = frames_read + frames
         _output, saved_params = logmmse(signal, fs, initial_noise, window_size, noise_threshold, saved_params)
-
-        _output = np.array(_output*np.iinfo(np.int16).max, dtype=np.int16)
-        output = np.concatenate((output, _output))
+        output = np.concatenate((output, from_float(_output, dtype)))
     write(output_filename, fs, output)
+
+run_logmmse('./test/pcm1608m.wav', 'sample2.wav')
